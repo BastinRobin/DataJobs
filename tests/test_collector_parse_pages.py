481b73f62a90b1fd273061d4fbb1ec4942a5f990
isaac obezo <isaac.obezo@gmail.com>
2014-07-02 17:06:31 -0700

    adding phantomjs to binaries

--- tests/test_collector_parse_pages.py
+++ tests/test_collector_parse_pages.py
@@ -1,15 +1,8 @@
 from nose.tools import ok_
-from nose.tools import raises
-from tests.utils import SAMPLE_SOURCE, out
-
+from tests.utils import SAMPLE_SOURCE
 from collector.parse_pages import Page
 
-
 PAGE = SAMPLE_SOURCE.read()
-XPATH = '//*[@id="SalaryChart"]/tbody[1]/tr[2]/td[1]/p[1]/a[2]/tt'
-XPATH = '//*[@id="SalaryChart"]/tbody[1]/tr'
-XPATH = '//*[@class="dataRow"]/td'
-XPATH = '//*[@class="i-occ"]/td'
 
 def test_collector_parse_pages_page():
     Page(PAGE)
--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,10 @@ from sys import stderr
 
 SAMPLE_SOURCE_HTML = os.path.join('tests', 'samples', 'sample_data_jobs.htm')
 SAMPLE_SOURCE = open(unicode(SAMPLE_SOURCE_HTML), 'rU')
+URL = 'http://www.google.com'
+HEADLESS_DRIVER = 'PhantomJS'
+PHANTOMJS = os.path.join(os.path.abspath(os.path.curdir), 'bin', 'phantomjs')
+
 
 def out(output):
     """ test util to output to stderr """
